6c292ab6bdf9726d25fe346d148443a09ddfd8b8
fccJashCoda <personalrevolution2014@gmail.com>
2020-11-27 01:32:03 +0100

    working timer prototype

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 
 const Timer = ({ startStop, reset, baseTime, paused }) => {
-  let date = new Date(0, 0, 0, 0, baseTime, 0);
+  // let date = new Date(0, 0, 0, 0, baseTime, 0);
+  const [date, setDate] = useState(new Date(0, 0, 0, 0, baseTime, 0));
   const [time, setTime] = useState(
     date.toLocaleTimeString(navigator.language, {
       minute: '2-digit',
@@ -10,27 +11,43 @@ const Timer = ({ startStop, reset, baseTime, paused }) => {
   );
 
   useEffect(() => {
+    setDate((prevState) => {
+      prevState = new Date(0, 0, 0, 0, baseTime, 0);
+
+      setTime(
+        prevState.toLocaleTimeString(navigator.language, {
+          minute: '2-digit',
+          second: '2-digit',
+        })
+      );
+      return prevState;
+    });
+    console.log(date);
+  }, [baseTime]);
+
+  useEffect(() => {
     let timer;
-    date = new Date(0, 0, 0, 0, baseTime, 0);
-    setTime(
-      date.toLocaleTimeString(navigator.language, {
-        minute: '2-digit',
-        second: '2-digit',
-      })
-    );
+    // date = new Date(0, 0, 0, 0, baseTime, 0);
+    // setTime(
+    //   date.toLocaleTimeString(navigator.language, {
+    //     minute: '2-digit',
+    //     second: '2-digit',
+    //   })
+    // );
     const countDown = () => {
       date.setSeconds(date.getSeconds() - 1);
+      console.log('inside date', date);
       setTime((prevState) => {
-        if (prevState === '24:55') {
-          date = new Date(0, 0, 0, 0, baseTime, 0);
-          console.log('shabang');
-        }
+        // if (prevState === '24:55') {
+        //   date = new Date(0, 0, 0, 0, baseTime, 0);
+        //   console.log('shabang');
+        // }
         prevState = date.toLocaleTimeString(navigator.language, {
           minute: '2-digit',
           second: '2-digit',
         });
-        console.log(prevState);
-        console.log(typeof prevState);
+        // console.log(prevState);
+        // console.log(typeof prevState);
         return prevState;
       });
     };
@@ -42,6 +59,7 @@ const Timer = ({ startStop, reset, baseTime, paused }) => {
     }
   }, [paused, baseTime]);
 
+  console.log('outside date', date);
   return (
     <div id="timer-label">
       <p>Session</p>
